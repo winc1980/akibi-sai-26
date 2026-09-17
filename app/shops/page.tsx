10571d6d85d1fffb3526d0544234351c4b0db2f5
hiroki307 <h.daina1730@gmail.com>
2026-09-17 11:19:51 +0900

    microCMSのセレクトボックスの返り値が配列の問題に対処、ファビコンを追加

--- app/shops/page.tsx
+++ app/shops/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <div>page</div>;
+import getMicroCmsData from "../microcms";
+
+export default async function Page() {
+  const shops = await getMicroCmsData("shops");
+
+  return (
+    <div>
+      {shops.filter((shop) => shop.category === "food").map((foodShop) => "A")}
+    </div>
+  );
 }
