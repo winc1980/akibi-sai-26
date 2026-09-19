7364b21a2a2bc1816f6baa69ae3316a768e2fad9
hattori-expertbook <hattori.index.js@gmail.com>
2026-09-19 12:54:25 +0900

    wip: 増野

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,3 +1,37 @@
-export default function Page() {
+import getMicroCmsData from "@/lib/microcms";
+
+const timeLabels = [
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+];
+
+const data = await getMicroCmsData("events");
+
+export default async function Page() {
   return <div>page</div>;
 }
+
+function isallDayEvents(x: number, y: number) {
+  const TIME_RANGE_START = 0;
+  const TIME_RANGE_END = 14;
+  return x === TIME_RANGE_START && y === TIME_RANGE_END;
+}
+
+const scheduledEvents = data.filter((data) => !isallDayEvents);
+const allDayEvents = data.filter((data) => isallDayEvents);
+const scheduledEventPlaces = Array.from(
+  new Set(scheduledEvents.map((event) => event.place)),
+);
