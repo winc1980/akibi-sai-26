3010cf910c07a1109b225f544214355fcef9707f
hattori-expertbook <hattori.index.js@gmail.com>
2026-09-19 10:53:35 +0900

    fix: aをNext.jsのLinkに /schedulesを/eventsに修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="font-zen-kaku fixed top-0 left-0 z-50 flex w-dvw justify-start pl-8 text-[1.3rem] font-bold">
-      <AnimationLink src="/たこさん.png" href="/">
+      <AnimationLink _src="/たこさん.png" href="/">
         トップ
       </AnimationLink>
-      <AnimationLink src="/たこさん.png" href="/maps">
+      <AnimationLink _src="/たこさん.png" href="/maps">
         マップ
       </AnimationLink>
-      <AnimationLink src="/たこさん.png" href="schedules">
+      <AnimationLink _src="/たこさん.png" href="/events">
         イベントスケジュール
       </AnimationLink>
-      <AnimationLink src="/たこさん.png" href="/shops">
+      <AnimationLink _src="/たこさん.png" href="/shops">
         模擬店
       </AnimationLink>
-      <AnimationLink src="/たこさん.png" href="exhibitions">
+      <AnimationLink _src="/たこさん.png" href="/exhibitions">
         展示
       </AnimationLink>
-      <AnimationLink src="/たこさん.png" href="sponsors">
+      <AnimationLink _src="/たこさん.png" href="/sponsors">
         協賛
       </AnimationLink>
     </header>
@@ -25,17 +27,17 @@ export default function Header() {
 function AnimationLink({
   children,
   href,
-  src,
+  _src,
 }: {
   children: string;
   href: string;
-  src: string;
+  _src: string;
 }) {
   //   const [isClicked, setIsClicked] = useState(false);
 
   return (
     <div className="text-default_black relative mr-5 flex h-16 items-center justify-center">
-      <a href={href}>{children}</a>
+      <Link href={href}>{children}</Link>
     </div>
   );
 }
