d26767ea74db4014fd22da22d48b5d1be06b63f8
hiroki307 <h.daina1730@gmail.com>
2026-09-24 00:45:31 +0900

    mapページの作成+github-cloudflare連携の確認

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <div>page</div>;
+import getMicroCmsData from "@/lib/microcms";
+
+export default async function Page() {
+  const data = await getMicroCmsData("constants");
+  if (data.map_img.length !== 1) {
+    throw new Error(`マップの画像が${data.map_img.length}枚ある`);
+  }
+  return (
+    <div className="font-yuji flex flex-col items-center justify-center gap-6 py-15 text-4xl sm:px-6 md:text-6xl xl:flex-row xl:text-[7rem]">
+      <p className="flex w-full items-center justify-start text-start md:justify-center">
+        学内マップ
+      </p>
+      <img
+        className="w-5xl max-w-dvw"
+        src={data.map_img[0].url}
+        alt="構内マップ"
+      />
+    </div>
+  );
 }
